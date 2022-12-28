74f02a0f18fd701b6ce1758002a30839ed63c4a1
syonfox <contact@kierlindsy.ca>
2022-12-27 19:02:29 -0500

    Updates starting to get the ball rolling

--- setup.sh
+++ setup.sh
@@ -3,18 +3,20 @@
 # Enable openssh server
 rc-update add sshd default
 
-# Configure networking
+# Configure networking for both public and vpc
 cat > /etc/network/interfaces <<-EOF
 iface lo inet loopback
 iface eth0 inet dhcp
+iface eth1 inet dhcp
 EOF
 
 ln -s networking /etc/init.d/net.lo
 ln -s networking /etc/init.d/net.eth0
+ln -s networking /etc/init.d/net.eth1
 
 rc-update add net.eth0 default
 rc-update add net.lo boot
-
+rc-update add net.eth1 boot
 # Create root ssh directory
 mkdir -p /root/.ssh
 chmod 700 /root/.ssh
@@ -23,8 +25,8 @@ chmod 700 /root/.ssh
 cat > /bin/do-init <<-EOF
 #!/bin/sh
 resize2fs /dev/vda
-wget -T 5 http://169.254.169.254/metadata/v1/hostname    -q -O /etc/hostname
-wget -T 5 http://169.254.169.254/metadata/v1/public-keys -q -O /root/.ssh/authorized_keys
+# wget -T 5 http://169.254.169.254/metadata/v1/hostname    -q -O /etc/hostname
+# wget -T 5 http://169.254.169.254/metadata/v1/public-keys -q -O /root/.ssh/authorized_keys
 hostname -F /etc/hostname
 chmod 600 /root/.ssh/authorized_keys
 rc-update del do-init default
