4c8c8ffe9eea6b3787a96ab1b4da83fc579db279
syonfox <contact@kierlindsy.ca>
2023-01-11 23:10:32 -0500

    add -t for console to -xen script

---
+++ build-image-xen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -o errexit
+
+F=alpine-virt-image-$(date +%Y-%m-%d-%H%M)
+
+if [ "$CI" = "true" ]
+then
+    echo "Running under CI"
+    echo $F > version
+fi
+
+./alpine-make-vm-image/alpine-make-vm-image --packages "openssh e2fsprogs-extra" -t --script-chroot --image-format qcow2 $F.qcow2 -- ./setup.sh
+bzip2 -z $F.qcow2
